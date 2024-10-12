d9c301df05100bd800574cf0e61f0674a485237a
Timofei Zhakov <tima@chemodax.net>
2024-10-12 16:46:05 +0200

    ImportVisualSVNServer.ps1 script: add documentation.

--- dist/Scripts/ImportVisualSVNServer.ps1
+++ dist/Scripts/ImportVisualSVNServer.ps1
@@ -1,3 +1,30 @@
+<#
+.SYNOPSIS
+Imports a certificate from WASC renewal into VisualSVN Server.
+
+.DESCRIPTION
+Note that this script is intended to be run via the install script plugin
+from WASC via the batch script wrapper. As such, we use positional parameters
+to avoid issues with using a dash in the cmd line.
+
+.PARAMETER PfxFile
+File name in the CertificatePath.
+
+.PARAMETER PfxPassword
+(Central Certificate Store) Password of the .pfx file.
+
+.EXAMPLE
+
+./Scripts/ImportVisualSVNServer.ps1 '{CacheFile}' '{CachePassword}'
+
+.NOTES
+Set-SvnServerConfiguration PowerShell function help:
+https://www.visualsvn.com/support/topic/00088/#Set-SvnServerConfiguration
+
+This script requires a clean installation of VisualSVN Server.
+
+#>
+
 param(
     [Parameter(Position = 0, Mandatory = $true)]
     [string]
